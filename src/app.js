3dc01c587023dd057f13474ee44494a907f78cf7
intellieseawolf <intelliseawolf103@gmail.com>
2023-01-28 11:00:15 +0800

    feat: importing ejs template

--- src/app.js
+++ src/app.js
@@ -1,13 +1,22 @@
 import express from "express";
-const port = 3333;
-const app = express();
+import path from "path";
+import { fileURLToPath } from "url";
 
-app.get("/", (req, res) => {
-    res.send("Hello from Zaptic! 👋💜");
+const port = 3000;
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set("views", path.join(__dirname, "/"));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", function (req, res) {
+  res.render("pages/index");
 });
 
 app.listen(port, () => {
-    console.log(`Listening: http://localhost:${port}`);
+  console.log(`Listening: http://localhost:${port}`);
 });
 
 export default app;
